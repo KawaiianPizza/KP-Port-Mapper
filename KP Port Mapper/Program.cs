8590b43a2b5966c5d726d7c9fe1d38448467aee9
Kawaiian Pizza <kawaiinpizza@gmail.com>
2021-09-19 22:22:17 -0700

    Revert "Fixed dpi scaling"
    This reverts commit f477c91fe37226541688d513e9b1e023cbf3d51e.

--- KP Port Mapper/Program.cs
+++ KP Port Mapper/Program.cs
@@ -14,7 +14,7 @@ namespace KP_Port_Mapper
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.DpiUnawareGdiScaled);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new formKPPortMapper());
